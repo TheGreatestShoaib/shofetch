0a55dc75c2a903940d9dffd4717b9605852b8e94
TheGreatestShoaib <shoaibmadeinbangladesh@gmail.com>
2021-09-23 16:10:56 +0600

    added new ascii

--- winfetch/__conf.py
+++ winfetch/__conf.py
@@ -2,7 +2,7 @@ from winfetch.sysmfy import *
 import winfetch.ascii_logos as logo
 
 
-banner_name = logo.arch
+banner_name = logo.small_arch
 sysfo = {}
 
 
@@ -13,7 +13,7 @@ sysfo["Kernel"] = kernel()
 sysfo["uptime"] = uptime
 sysfo["Resolution"] = screensize()
 sysfo["Architecure"] = processor_architecture()
-#sysfo["Cpus-Freq"] = cpu_frequency()[0]/100
+sysfo["Cpus-Freq"] = cpu_frequency()[0]/100
 sysfo["Cpu_model"] = cpu_name()
 sysfo["GPU_model"] = gpu_name()
 sysfo["memory"] = f"{available_memory} / {total_memory} ( {memory_usage_percent}% )"
