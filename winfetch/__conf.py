90ee609bbe2260d4e6f8a13e0e8e269ed8f8b32e
TheGreatestShoaib <shoaibmadeinbangladesh@gmail.com>
2021-09-24 20:28:48 +0600

    Fixed Layout Issue

--- winfetch/__conf.py
+++ winfetch/__conf.py
@@ -6,17 +6,17 @@ banner_name = logo.small_arch
 sysfo = {}
 
 
-#sysfo["Platform"] = platform_name()
-sysfo["Host_name"] = host_name().node
-sysfo["OS"] = os_version()
-sysfo["Kernel"] = kernel()
-sysfo["uptime"] = uptime
-sysfo["Resolution"] = screensize()
-sysfo["Architecure"] = processor_architecture()
-sysfo["Cpus-Freq"] = cpu_frequency()[0]/100
-sysfo["Cpu_model"] = cpu_name()
-sysfo["GPU_model"] = gpu_name()
-sysfo["memory"] = f"{available_memory} / {total_memory} ( {memory_usage_percent}% )"
+sysfo["Platform 	"] = platform_name()
+sysfo["Host_name 	"] = host_name().node
+sysfo["OS 		"] = os_version()
+sysfo["Kernel		"] = kernel()
+sysfo["uptime 		"] = uptime
+sysfo["Resolution	"] = screensize()
+sysfo["Architecure 	"] = processor_architecture()
+sysfo["Cpus-Freq	"] = cpu_frequency()[0]/100
+sysfo["Cpu_model	"] = cpu_name()
+sysfo["GPU_model	"] = gpu_name()
+sysfo["memory		"] = f"{available_memory} / {total_memory} ( {memory_usage_percent}% )"
 
 
 
